706dad0fbde4fff5056af613999b85ce5f090d2b
Lauro Tremea Culau <laurotc@gmail.com>
2019-10-09 23:38:16 -0300

    Adicionar uso do serviço no manifest e na main activity
    Signed-off-by: Lauro Tremea Culau <laurotc@gmail.com>

--- app/src/main/java/com/example/doubletap/MainActivity.java
+++ app/src/main/java/com/example/doubletap/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.doubletap;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent serviceIntent = new Intent(MainActivity.this, SensorActivity.class);
+        startService(serviceIntent);
     }
 }
